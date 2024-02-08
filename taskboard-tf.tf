ed2623dc7f59e07ca57ddd5d2910f527d3176796
YoanLigov2 <140204865+YoanLigov2@users.noreply.github.com>
2024-02-08 15:23:29 +0200

    Update taskboard-tf.tf

--- taskboard-tf.tf
+++ taskboard-tf.tf
@@ -5,6 +5,13 @@ terraform {
       version = "=3.0.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "StorageRG"
+    storage_account_name = "taskboardstorageyoanli"
+    container_name       = "taskboardyoanlicontainer"
+    key                  = "terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
